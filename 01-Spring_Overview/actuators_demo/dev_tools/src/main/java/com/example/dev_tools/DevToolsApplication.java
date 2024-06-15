1c00c0b7822e1638878b5f1c2be46379cdc2df7e
Pratik <dhanepratik543@gmail.com>
2024-06-15 18:57:51 +0530

    Spring Overview

---
+++ 01-Spring_Overview/actuators_demo/dev_tools/src/main/java/com/example/dev_tools/DevToolsApplication.java
@@ -0,0 +1,12 @@
+package com.example.dev_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(DevToolsApplication.class, args);
+	}
+}
